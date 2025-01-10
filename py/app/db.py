1ddfd726850af8c1944cb54772449e183b9fe888
周凯 <851708184@qq.com>
2025-01-10 18:00:27 +0800

    feat: 添加mongodb依赖

---
+++ py/app/db.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from pymongo import MongoClient
+
+def get_database():
+    user = "zhoukai"
+    passwd = "123456"
+    host = "192.168.192.3"
+    port = "27017"
+    CONNECTION_STRING = f"mongodb://{user}:{passwd}@{host}:{port}"
+    client = MongoClient(CONNECTION_STRING)
+    return client
+
+if __name__ == "__main__":
+    dbname = get_database()
+    print(dbname)
+    pass
