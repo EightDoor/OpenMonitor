926808e5308b75bb73a719bd0285d07077fa97e0
eightdoor <851708184@qq.com>
2025-01-13 23:25:51 +0800

    fix: 打印日志级别

--- py/app/logger.py
+++ py/app/logger.py
@@ -2,10 +2,8 @@ import logging
 import os
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
 
 
 def init():
-    path = os.path.abspath("../")
-    print(path)
-    logging.basicConfig("")
+    logging.basicConfig(level=logging.DEBUG)
+
