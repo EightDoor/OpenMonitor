271f42d869715c50b067727377f87f1376c39988
eightdoor <851708184@qq.com>
2025-01-15 22:37:38 +0800

    feat: 支持跨域

--- py/main.py
+++ py/main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
 from app import router, logger
+from fastapi.middleware.cors import CORSMiddleware
 from app.config import APP_RELOAD
 import uvicorn
 
 app = FastAPI()
 
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 logger.init()
 app.include_router(router.router)
 
