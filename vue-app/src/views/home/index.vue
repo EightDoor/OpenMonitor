e4660b76e04476eb7f299542377d5aaeda535401
周凯 <851708184@qq.com>
2025-01-15 22:19:53 +0800

    fix

--- vue-app/src/views/home/index.vue
+++ vue-app/src/views/home/index.vue
@@ -1,9 +1,9 @@
 <template>
   <HomeLayout>
     <el-tabs v-model="activeName" class="demo-tabs" @tab-click="handleClick">
-      <!--      <el-tab-pane label="基础" name="sysinfo">-->
-      <!--        <SysInfoMonitor/>-->
-      <!--      </el-tab-pane>-->
+      <el-tab-pane label="基础" name="sysinfo">
+        <SysInfoMonitor/>
+      </el-tab-pane>
       <el-tab-pane label="设置" name="setting">
         <Settings/>
       </el-tab-pane>
@@ -16,9 +16,11 @@ import HomeLayout from "@/views/home/components/HomeLayout.vue";
 import {ref} from "vue";
 import Settings from "@/views/home/components/Settings/Settings.vue";
 import {TabsPaneContext} from "element-plus";
+import SysInfoMonitor from "@/views/home/components/SysInfo/SysInfoMonitor.vue";
 
-// const activeName = ref("sysinfo")
-const activeName = ref("setting")
+const activeName = ref("sysinfo")
+
+// const activeName = ref("setting")
 
 function handleClick(tab: TabsPaneContext, event: Event) {
   console.log(tab, event)
