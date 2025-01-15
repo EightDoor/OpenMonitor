02c6dbcd090ad808f585f373900edb9ddc96f5b7
周凯 <851708184@qq.com>
2025-01-15 22:23:04 +0800

    chore: 修改打包配置

--- vue-app/src/config/index.ts
+++ vue-app/src/config/index.ts
@@ -1,5 +1,11 @@
 const Config = {
-    baseUrl: "/api"
+    baseUrl: "/api",
+    proUrl: "http://192.168.192.3:8500"
+}
+
+const pro = import.meta.env.PROD
+if (pro) {
+    Config.baseUrl = Config.proUrl
 }
 
 export default Config
